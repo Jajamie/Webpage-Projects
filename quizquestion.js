5dc52dd0192cbd2b278daebc1dbd2db67e7bcc0d
Jajamie <jamierose06132007@gmail.com>
2024-05-12 00:41:36 +0800

    Webpage Projects

---
+++ quizquestion.js
@@ -0,0 +1,191 @@
+let questions = [
+    {
+        numb:1,
+        question:"What is the primary male sex hormone?",
+        answer:"C. Testosterone",
+        options:[
+            "A. Estrogen",
+            "B. Progesterone", 
+            "C. Testosterone", 
+            "D. Prolactin"
+
+        ]
+        
+    },
+    {
+        numb:2,
+        question:"Where are sperm produced in the male reproductive system",
+        answer:"A. Testes",
+        options:[
+            "A. Testes", 
+            "C. Epididymis", 
+            "C. Vas Deferens", 
+            "D. Urethra"
+            
+        ]
+        
+    },
+    {
+        numb:3,
+        question:"What is the function of the prostate gland?",
+        answer:"C. Produces semen",
+        options:[
+            "A. Produces sperm",
+            "B. Produces testosterone", 
+            "C. Produces semen", 
+            "D. Produces urine"
+            
+        ]
+        
+    },
+    {
+        numb:4,
+        question:"What is the tube that carries sperm from the testes to the urethra?",
+        answer:"B. Vas Deferens",
+        options:[
+            "A. Epididymis", 
+            "B. Vas Deferens", 
+            "C. Seminal Vesicle", 
+            "D. Bulbourethral Gland"
+        ]
+        
+    },
+    {
+        numb:5,
+        question:"What is the external male reproductive organ?",
+        answer:"A. Penis",
+        options:[
+            "A. Penis", 
+            "B. Scrotum", 
+            "C. Prostate Gland", 
+            "D. Seminal Vesicle"
+        ]
+        
+    },
+    {
+        numb:6,
+        question:"Which one of the following is not part of the male reproductive system?",
+        answer:"B. Bladder",
+        options:[
+            "A. Scrotum",
+            "B. Bladder",
+            "C. Prostrate gland",
+            "D. Penis",
+        ]
+        
+    },
+    {
+        numb:7,
+        question:"A major male sex organ that produces and stores sperm.",
+        answer:"B. Testicle",
+        options:[
+            
+            "A. Prostate",
+            "B. Testicle",
+            "C. Ovary",           
+            "D. Scrotum",
+                
+        ]
+        
+    },
+    {
+        numb:8,
+        question:"A set of coiled tubes that connect to the vas deferens.",
+        answer:"B. Epididymis",
+        options:[
+            
+            "A. Fallopian tubes",
+            "B. Epididymis",
+            "C. Urethra",
+            "D. Prostate",
+        ]
+        
+    },
+    {
+        numb:9,
+        question:"A muscular tube that transports sperm-filled semen.",
+        answer:"C. Vas deferens",
+        options:[
+            
+            "A. Vagina",
+            "B. Fallopian tube",
+            "C. Vas deferens",
+            "D. Pubic bone",
+        ]
+        
+    },
+
+    {
+        numb:10,
+        question:"The sac that holds the testes.",
+        answer:"A. Scrotum",
+        options:[
+            "A. Scrotum",
+            "B. Placenta",
+            "C. Vas deferens",
+            "D. Testis",
+        ]
+        
+    },
+    {
+        numb:11,
+        question:"A process in which sperm-laden semen leaves the male body.",
+        answer:"C. Ejaculation",
+        options:[
+            "A. Ovulation",
+            "B. Gestation",
+            "C. Ejaculation",
+            "D. Urination",
+        ]
+        
+    },
+    {
+        numb:12,
+        question:"A gland that makes some of the parts of semen.",
+        answer:"C. Prostate",
+        options:[
+            "A. Ovary",
+            "B. Scrotum",
+            "C. Prostate",
+            "D. Bulbourethral",
+        ]
+        
+    },
+    {
+        numb:13,
+        question:"Where does sperm maturation occur",
+        answer:"B. Epididymis",
+        options:[
+            "A. Seminiferous tubules",
+            "B. Epididymis",
+            "C. Prostate gland",
+            "D. Bulbourethral gland",
+        ]
+        
+    },
+    {
+        numb:14,
+        question:"Which gland produces a fluid that nourishes and protects sperm",
+        answer:"B. Seminal vesicles",
+        options:[
+            "A. Prostate gland",
+            "B. Seminal vesicles",
+            "C. Bulbourethral gland",
+            "D. Cowper's gland",
+        ]
+        
+    },
+    {
+        numb:15,
+        question:"Which structure surrounds the urethra and produces an alkaline fluid",
+        answer:"A. Prostate gland",
+        options:[
+            "A. Prostate gland",
+            "B. Seminal vesicles",
+            "C. Bulbourethral gland",
+            "D. Epididymis",
+        ]
+        
+    },
+    
+];
